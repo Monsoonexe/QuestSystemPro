ff63885989b7255de95cace01a5edf489d52b03c
Richard Osborn <richardosborn256@gmail.com>
2025-04-15 00:00:34 -0700

    Pick object picker fighting singletons

--- Scripts/Quests/Editor/QuestPickerEditorWindow.cs
+++ Scripts/Quests/Editor/QuestPickerEditorWindow.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using Devdog.General.Editors;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using Devdog.General.Editors;
-using Devdog.General.Editors.ReflectionDrawers;
 using UnityEditor;
 using UnityEngine;
 using Object = UnityEngine.Object;
@@ -21,17 +20,23 @@ namespace Devdog.QuestSystemPro.Editors
 
         protected override IEnumerable<Object> FindAssetsOfType(Type type, bool allowInherited)
         {
+            return AssetDatabase.FindAssets("t:" + typeof(Quest).FullName) // Devdog.QuestSystemPro.Quest
+                .Select(guid => AssetDatabase.GUIDToAssetPath(guid))
+                .Select(path => AssetDatabase.LoadAssetAtPath<Quest>(path));
+
+            /* singleton isn't reliable in editor, with domain reload, etc.
             if (QuestManager.instance == null)
             {
-                return new Object[0];
+                return System.Array.Empty<Object>();
             }
 
             return QuestManager.instance.quests;
+            */
         }
 
         protected override IEnumerable<Object> FindAssetsWithComponent(Type type, bool allowInherited)
         {
-            return new Object[0];
+            return System.Array.Empty<Object>();
         }
 
         public override bool IsSearchMatch(Object asset, string searchQuery)
@@ -43,7 +48,7 @@ namespace Devdog.QuestSystemPro.Editors
                 return q.name.message.ToLower().Contains(searchQuery) ||
                        q.description.message.ToLower().Contains(searchQuery) ||
                        q.ID.ToString().Contains(searchQuery);
-//                       q.tasks.Any(o => o.key.ToLower().Contains(searchQuery) || o.description.ToLower().Contains(searchQuery))
+                //                       q.tasks.Any(o => o.key.ToLower().Contains(searchQuery) || o.description.ToLower().Contains(searchQuery))
             }
 
             return base.IsSearchMatch(asset, searchQuery);
@@ -56,7 +61,7 @@ namespace Devdog.QuestSystemPro.Editors
                 var q = asset as Quest;
                 if (q != null)
                 {
-                    var cellSize = r.width;
+                    float cellSize = r.width;
 
                     var labelRect = new Rect(5, 5, cellSize, EditorGUIUtility.singleLineHeight);
                     GUI.Label(labelRect, asset.GetType().Name, UnityEditor.EditorStyles.boldLabel);
@@ -66,9 +71,6 @@ namespace Devdog.QuestSystemPro.Editors
 
                     labelRect.y += EditorGUIUtility.singleLineHeight;
                     GUI.Label(labelRect, q.tasks.Length + " tasks");
-
-//                    var iconSize = Mathf.RoundToInt(cellSize * 0.6f);
-//                    GUI.DrawTexture(new Rect(cellSize * 0.2f, cellSize * 0.4f - InnerPadding, iconSize, iconSize), AssetDatabase.GetCachedIcon(AssetDatabase.GetAssetPath(asset)));
 
                     return;
                 }
